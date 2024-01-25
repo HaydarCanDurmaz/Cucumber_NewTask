5db1c47faeb54df172d6d7ee4512c740ca075d04
HaydarCanDurmaz <candrhydr@hotmail.com>
2024-01-25 19:34:32 +0300

    Reggession Test and Smoke TEst Runner With Plugin Basic Site

---
+++ src/test/java/Runners/_06_TestRunnerSomkeWithPlugin.java
@@ -0,0 +1,15 @@
+package Runners;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+        tags = "@Regression and @SmokeTest", // hangi senaryolarda bu etiket varsa onlar çalıştırılacak
+        // or yerine and yazılırsa hem smoke hem de regression tag i olanları çalıştırıyor
+        features = {"src/test/java/FeatureFiles"},
+        glue = {"StepDefinitios"},
+        plugin = ("html:target/site/cucumber-pretty.html")
+
+)
+public class _06_TestRunnerSomkeWithPlugin extends AbstractTestNGCucumberTests {
+}
