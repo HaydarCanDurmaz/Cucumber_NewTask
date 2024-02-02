75b732251ef972c936f110e91f50465e50fc2baa
HaydarCanDurmaz <candrhydr@hotmail.com>
2024-02-02 10:51:34 +0300

    _10_ApachePOISteps

---
+++ src/test/resources/StepDefinitions/_10_ApachePOISteps.java
@@ -0,0 +1,10 @@
+package StepDefinitions;
+
+import io.cucumber.java.en.Then;
+
+public class _10_ApachePOISteps {
+    @Then("User Delete citizenship with ApacehPOI")
+    public void userDeleteCitizenshipWithApacehPOI() {
+
+    }
+}
