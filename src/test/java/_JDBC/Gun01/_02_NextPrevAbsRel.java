92c56c064f52d1710b79c73e936d5738ae8d5789
HaydarCanDurmaz <candrhydr@hotmail.com>
2024-03-01 00:13:18 +0300

    AbsoluteRelative bulundugu yerden verilen yere gider

--- src/test/java/_JDBC/Gun01/_02_NextPrevAbsRel.java
+++ src/test/java/_JDBC/Gun01/_02_NextPrevAbsRel.java
@@ -23,4 +23,30 @@ public class _02_NextPrevAbsRel extends _JDBC_Parent {
         rs.previous(); // 2 satıra geri gitti
         System.out.println("2.satır DilAdi=" + rs.getString(2));// italien
     }
+    @Test
+    public void AbsoluteRelative() throws SQLException {
+        ResultSet rs = statement.executeQuery("select * from film");
+
+        // rs.next() : sonraki satır
+        // rs.previous : önceki satır
+        // rs.absolute(10) : baştan itibaren 10.satıra gider.
+        // rs.relative(10) : bulunduğu yerden 10.satıra gider.
+        // rs.absolute(-10) : - (eksi) sondan demektir, sondan 10.satır
+        // rs.relative(-5) :  - (eksi) bulundu yerden 5 satır geri
+
+        rs.absolute(10);  // 10.satır git
+        System.out.println("10.satır - title = " + rs.getString("title"));
+
+        //rs.absolute(5); // 5.satıra giderdi.
+
+        rs.relative(5);  // 15.satıra gider. Niye? çünkü en son bulunduğu yerden 5 satır gider
+        System.out.println("15.satır - title = " + rs.getString("title"));
+
+        rs.absolute(-10); // sondan 10.satır a git
+        System.out.println("sondan 10.satır - title = " + rs.getString("title"));
+
+
+
+
+    }
 }
